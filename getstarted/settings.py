7d73ab55258438bc20fef85ac9be064be4cf0241
Varal7 <varaal@gmail.com>
2016-01-19 22:03:14 +0100

    Update settings file

--- getstarted/settings.py
+++ getstarted/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event',
+    'wiget_tweaks',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/static/")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "assets/media/")
+
+MEDIA_URL = '/assets/media/'
