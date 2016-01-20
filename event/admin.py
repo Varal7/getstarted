e91870c9c5da16e31d7a633e69b4ae985cad7271
Varal7 <varaal@gmail.com>
2016-01-20 18:19:32 +0100

    Removed unused models from admin interface

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import *
 # Register your models here.
 
 from .models import Participant
 
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Participant)
