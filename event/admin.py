ca924758df681dc22c9b3ca12c1ff487076b3730
Varal7 <varaal@gmail.com>
2016-01-22 00:05:55 +0100

    Updated admin interface

--- event/admin.py
+++ event/admin.py
@@ -7,5 +7,16 @@ from .models import Participant, Event
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
-admin.site.register(Participant)
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    fields = ('name', 'is_active', 'hits')
+    readonly_fields = ('hits','name')
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_joined',  'email',  'cv')
+    fields = ('first_name', 'last_name', 'date_joined',  'email',  'cv', 'year_in_school', 'promo', 'found_stage', 'domains', 'missions', 'want_cocktail', 'is_active')
+    readonly_fields = ('first_name', 'last_name', 'date_joined',  'email', 'promo', 'cv', 'promo', 'year_in_school', 'found_stage', 'domains', 'missions', 'want_cocktail')
+
+
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Event, EventAdmin)
